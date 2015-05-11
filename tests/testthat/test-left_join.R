1849cb1d000c64bab84b386ca5a0b2a87c960e0b
Wush Wu <wush978@gmail.com>
2015-05-11 21:33:01 +0800

    test left_join

---
+++ tests/testthat/test-left_join.R
@@ -0,0 +1,30 @@
+context("left_join")
+
+test_that("left_join works correctly", {
+  
+  src <- src_spark_sql("localhost", "10000", Sys.info()["nodename"])
+  
+  tbs <- db_list_tables(src)
+  if (length(tbs) > 0) {
+    for(i in seq_along(tbs)) stopifnot(db_has_table(src, tbs[i]))
+    for(i in seq_along(tbs)) db_drop_table(src, tbs[i])
+  }
+  db_create_table(src, "people", stored_as = "JSON", temporary = TRUE, 
+                  location = sprintf("file://%s", system.file("examples/people.json", package = "dplyrSparkSQL")))
+  
+  people <- tbl(src, "people")
+  
+  db_create_table(src, "users", stored_as = "PARQUET", temporary = TRUE, 
+                  location = sprintf("file://%s", system.file("examples/users.parquet", package = "dplyrSparkSQL")))
+  users <- tbl(src, "users")
+  spark_result <- left_join(people, users, by = "name") %>% arrange(name) %>% collect %>% as.data.frame
+  local_result <- left_join(people %>% collect, users %>% collect, by = "name") %>% arrange(name) %>% as.data.frame
+  expect_equal(spark_result, local_result)
+
+  tbs <- db_list_tables(src)
+  if (length(tbs) > 0) {
+    for(i in seq_along(tbs)) stopifnot(db_has_table(src, tbs[i]))
+    for(i in seq_along(tbs)) db_drop_table(src, tbs[i])
+  }
+  
+})
