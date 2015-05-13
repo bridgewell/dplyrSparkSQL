a2d6b8c68a8afc53245b6a4c11e88726259bca80
Wush Wu <wush978@gmail.com>
2015-05-13 20:55:09 +0800

    debug: logic error

--- inst/run_test.sh
+++ inst/run_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 PACKAGE_PATH=`pwd`
-ls /tmp/dplyrSparkSQLTestServer || rm -rf /tmp/dplyrSparkSQLTestServer
+ls /tmp/dplyrSparkSQLTestServer && rm -rf /tmp/dplyrSparkSQLTestServer
 git clone https://github.com/wush978/dplyrSparkSQLTestServer.git /tmp/dplyrSparkSQLTestServer
 cd /tmp/dplyrSparkSQLTestServer && sbt "run $PACKAGE_PATH" || mv /tmp/dplyrSparkSQLTestServer/dplyrSparkSQL.Rcheck
