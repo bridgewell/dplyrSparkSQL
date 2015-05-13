178e8f3aec9e9005d46d5af1326e04425c48516b
Wush Wu <wush978@gmail.com>
2015-05-13 20:59:59 +0800

    debug: mv: missing destination file operand after ‘/tmp/dplyrSparkSQLTestServer/dplyrSparkSQL.Rcheck’

--- inst/run_test.sh
+++ inst/run_test.sh
@@ -3,4 +3,4 @@
 PACKAGE_PATH=`pwd`
 ls /tmp/dplyrSparkSQLTestServer && rm -rf /tmp/dplyrSparkSQLTestServer
 git clone https://github.com/wush978/dplyrSparkSQLTestServer.git /tmp/dplyrSparkSQLTestServer
-cd /tmp/dplyrSparkSQLTestServer && sbt "run $PACKAGE_PATH" || mv /tmp/dplyrSparkSQLTestServer/dplyrSparkSQL.Rcheck
+cd /tmp/dplyrSparkSQLTestServer && sbt "run $PACKAGE_PATH" || mv /tmp/dplyrSparkSQLTestServer/dplyrSparkSQL.Rcheck .
