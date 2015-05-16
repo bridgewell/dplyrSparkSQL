2d68aa658ab2c85dc9b46385ecf816919cec591b
Wush Wu <wush978@gmail.com>
2015-05-16 13:18:27 +0800

    Revert "adopt new execution method in test server"
    This reverts commit 267a932400e8203eaa5b14b0ff35a3f0ed7d7e49.

--- inst/run_test.sh
+++ inst/run_test.sh
@@ -3,4 +3,4 @@
 PACKAGE_PATH=`pwd`
 ls /tmp/dplyrSparkSQLTestServer && rm -rf /tmp/dplyrSparkSQLTestServer
 git clone https://github.com/wush978/dplyrSparkSQLTestServer.git /tmp/dplyrSparkSQLTestServer
-cd /tmp/dplyrSparkSQLTestServer && sbt "myRun $PACKAGE_PATH"
+cd /tmp/dplyrSparkSQLTestServer && sbt "myRun $PACKAGE_PATH" || mv /tmp/dplyrSparkSQLTestServer/dplyrSparkSQL.Rcheck .
